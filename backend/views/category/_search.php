9c8726770af0c555fcdb7f1a8df9b00badbab32c
QuanTien <vinhnt9x@gmail.com>
2017-04-18 14:00:08 +0700

    update

---
+++ backend/views/category/_search.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\CategorySearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="category-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'cateName') ?>
+
+    <?= $form->field($model, 'parent_id') ?>
+
+    <?= $form->field($model, 'keywords') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?php // echo $form->field($model, 'order') ?>
+
+    <?php // echo $form->field($model, 'groups_id') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <?php // echo $form->field($model, 'created_at') ?>
+
+    <?php // echo $form->field($model, 'updated_at') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
